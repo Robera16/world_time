1e29b038a76e7dad01a481c5277fe0c25c60d635
Robera16 <roberaworknehrobwork@gmail.com>
2022-09-19 20:25:34 +0300

    map&routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'pages/loading.dart';
 import 'pages/home.dart';
+import 'pages/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location':(context) => ChooseLocation()
+    },
   ));
 }
 
